3b1d99198e4c87c4d5cdfaad27308ef4f1d5226a
jlauza <lauzajayson@gmail.com>
2024-09-28 16:41:08 +0800

    generate id and secret

--- src/app/api/auth/[...nextauth]/options.jsx
+++ src/app/api/auth/[...nextauth]/options.jsx
@@ -8,17 +8,17 @@ export const options = {
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      authorization: {
-        params: {
-          prompt: "consent",
-          access_type: "offline",
-          response_type: "code",
-        },
-      },
-    }),
+    // GoogleProvider({
+    //   clientId: process.env.GOOGLE_CLIENT_ID,
+    //   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    //   authorization: {
+    //     params: {
+    //       prompt: "consent",
+    //       access_type: "offline",
+    //       response_type: "code",
+    //     },
+    //   },
+    // }),
     CredentialsProvider({
       name: "Credentials",
       credentials: {
