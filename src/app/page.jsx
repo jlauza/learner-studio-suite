9de06b806ac040f8f4dcf85a528113569eef2cc8
jlauza <lauzajayson@gmail.com>
2024-10-05 15:36:51 +0800

    website designs

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,6 +2,11 @@ import { options } from "./api/auth/[...nextauth]/options";
 import { getServerSession } from "next-auth/next";
 import HomeNavbar from "@/components/home-navbar";
 import MarketingHighlight from "@/components/marketing-highlight";
+import BentoGrid from "@/components/bento-grid";
+import MeetTeam from "@/components/meet-team";
+import WorkWithUs from "@/components/work-with-us";
+import Pricing from "@/components/pricing";
+import ContactSales from "@/components/contact-sales";
 import Footer from "@/components/footer";
 
 export default async function Home() {
@@ -11,6 +16,11 @@ export default async function Home() {
     <>
       <HomeNavbar session={session} />
       <MarketingHighlight />
+      <BentoGrid />
+      <WorkWithUs />
+      <MeetTeam />
+      <Pricing />
+      <ContactSales />
       <Footer />
     </>
   );
