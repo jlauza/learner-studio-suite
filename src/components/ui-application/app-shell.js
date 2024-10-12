86b4b4a3a3a2a421e74961fd3dfa5370a5275a63
jlauza <lauzajayson@gmail.com>
2024-10-13 00:03:55 +0800

    full width

--- src/components/ui-application/app-shell.js
+++ src/components/ui-application/app-shell.js
@@ -50,7 +50,7 @@ export default async function AppShell({ children, title }) {
     <>
       <div className="min-h-full">
         <Disclosure as="nav" className="bg-gray-800">
-          <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+          <div className="mx-auto px-4 sm:px-6 lg:px-8">
             <div className="flex h-16 items-center justify-between">
               <div className="flex items-center">
                 <div className="flex-shrink-0">
@@ -203,9 +203,7 @@ export default async function AppShell({ children, title }) {
 
         <AppNavbar title={title} />
         <main>
-          <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-            {children}
-          </div>
+          <div className="mx-auto px-4 py-6 sm:px-6 lg:px-8">{children}</div>
         </main>
       </div>
     </>
