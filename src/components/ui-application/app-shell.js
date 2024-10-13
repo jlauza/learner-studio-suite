7a575fb3f7a03f94123ce69bff357e83679c5895
jlauza <lauzajayson@gmail.com>
2024-10-13 14:05:20 +0800

    revert

--- src/components/ui-application/app-shell.js
+++ src/components/ui-application/app-shell.js
@@ -6,17 +6,6 @@ import AppSidebar from "./app-sidebar";
 export default function AppShell({ children, title }) {
   const [sidebarOpen, setSidebarOpen] = React.useState(true);
 
-  React.useEffect(() => {
-    const sideBarState = localStorage.getItem("sidebarOpen");
-    if (sideBarState !== null) {
-      setSidebarOpen(JSON.parse(sideBarState));
-    }
-  }, []);
-
-  React.useEffect(() => {
-    localStorage.setItem("sidebarOpen", JSON.stringify(sidebarOpen));
-  }, []);
-
   function clickMe() {
     setSidebarOpen((prevState) => !prevState);
   }
